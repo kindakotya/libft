3abd180f1de0b746419bfbc7226fc8971611c3b5
gmayweat <gmayweat@student.21-school.ru>
2021-02-22 16:20:46 +0300

    new

---
+++ src/ft_memccpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmayweat <gmayweat@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 17:23:01 by gmayweat          #+#    #+#             */
+/*   Updated: 2021/01/18 13:52:18 by gmayweat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char*)dest)[i] = ((unsigned char*)src)[i];
+		if (((unsigned char*)src)[i] == (unsigned char)c)
+			return (dest + 1 + i);
+		++i;
+	}
+	return (NULL);
+}
